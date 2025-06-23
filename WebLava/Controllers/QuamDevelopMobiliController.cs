17bf1fb004f51370ddc42b41a6743ecbcc61fb8c
ashurkovav <ashurkovav@yandex.ru>
2025-06-23 17:23:34 +0300

    +добавил статьи и цены

---
+++ WebLava/Controllers/QuamDevelopMobiliController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLava.Controllers
+{
+    public class QuamDevelopMobiliController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Blog/QuamDevelopMobili/Index.cshtml");
+        }
+    }
+}
---
+++ WebLava/Controllers/SinglePostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLava.Controllers
+{
+    public class SinglePostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
