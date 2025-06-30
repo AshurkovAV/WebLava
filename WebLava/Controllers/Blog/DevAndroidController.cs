74ec10eaf43ab937973224c3395d85b804e98d7d
ashurkovav <ashurkovav@yandex.ru>
2025-06-30 20:25:26 +0300

    +добавил еще один пост в блог

---
+++ WebLava/Controllers/Blog/DevAndroidController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLava.Controllers.Blog
+{
+    [Route("Blog/DevAndroid")]
+    public class DevAndroidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Blog/DevAndroid/Index.cshtml");
+        }
+    }
+}
