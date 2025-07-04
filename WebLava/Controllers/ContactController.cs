733d6c4793201716a7c290cff72d032eb7270914
ashurkovav <ashurkovav@yandex.ru>
2025-07-04 23:31:30 +0300

    +исправил ViewData["Description"]

--- WebLava/Controllers/ContactController.cs
+++ WebLava/Controllers/ContactController.cs
@@ -1,10 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebLava.Models;
 
 namespace WebLava.Controllers
 {
     public class ContactController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SubmitForm(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                    Console.WriteLine(model?.Name);
+                    Console.WriteLine(model?.Emailid);
+                    Console.WriteLine(model?.Msg);
+                    Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"); 
+
+                    if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                    {
+                        // Это AJAX запрос - возвращаем JSON
+                        return Json(new { success = true, message = "Сообщение успешно отправлено!" });
+                    }
+                    else
+                    {
+                        // Обычный запрос - устанавливаем TempData и редиректим
+                        TempData["SuccessMessage"] = "Сообщение успешно отправлено!";
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch
+                {
+                    if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                    {
+                        return Json(new { success = false, message = "Произошла ошибка при отправке." });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Произошла ошибка при отправке.");
+                    }
+                }
+                
+
+                
+            }
+
+            // Если есть ошибки валидации
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return Json(new { success = false, message = "Пожалуйста, исправьте ошибки в форме." });
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Success()
         {
             return View();
         }
---
+++ WebLava/Models/ContactFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebLava.Models
+{
+    public class ContactFormModel
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите ваше имя")]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите ваш email")]
+        [EmailAddress(ErrorMessage = "Пожалуйста, введите корректный email")]
+        [Display(Name = "Email")]
+        public string Emailid { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите сообщение")]
+        [Display(Name = "Сообщение")]
+        public string Msg { get; set; }
+    }
+}
